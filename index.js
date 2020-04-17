832e533459d00c3b9f47d698b26a1685d578d71b
18521604 <18521604@gm.uit.edu.vn>
2020-04-17 16:22:38 +0700

    Add carts

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ var cookieParser = require('cookie-parser')
 var userRoute = require('./routes/user.route');
 var authRoute = require('./routes/auth.route');
 var productRoute = require('./routes/product.route');
+var cartRoute = require('./routes/cart.route');
 
-var middleWare = require('./middleware/auth.middleware');
+var authMiddleware = require('./middleware/auth.middleware');
+var sessionMiddleware = require('./middleware/session.middleware');
 
 var port = 3001;
 
@@ -17,6 +19,7 @@ app.set('views', './views');
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SECTION_SECRET))
+app.use(sessionMiddleware);
 
 app.use(express.static('public'))
 
@@ -24,9 +27,10 @@ app.get('/',function(req, res) {
     res.render('index');
 });
 
-app.use('/users', middleWare.requireAuth, userRoute);
+app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
 app.use('/products', productRoute);
+app.use('/cart', cartRoute);
 
 app.listen(port, function() {
     console.log("Server listening on port: " + port);
